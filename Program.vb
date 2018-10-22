c5abd938b5d7aa97d02e640480dc7ae92743e2b4
Randall-Adams <44162048+Randall-Adams@users.noreply.github.com>
2018-10-22 07:38:54 +0000

    The Global Namespace of Kairen
    The Global things that need to persist outside of any window/other code.

---
+++ Program.vb
@@ -0,0 +1,15 @@
+﻿Module Program
+    Public ReadOnly ProgramVersionNumber As String = "4.0.0.0"
+    Public CurrentUserName As String
+    Public Folders As New classFolders
+    Public Files As New classFiles
+    Friend Class classFolders
+        Public ReadOnly AppData As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\Kairen4\"
+        Public ReadOnly AccountsFolder As String = AppData & "Account Data\"
+        Public CurrentAccount As String
+    End Class
+    Friend Class classFiles
+        Public WithEvents ProgramSettings As New Class_TextFile(Folders.AppData, "ProgramSettings.txt", True)
+        Public WithEvents UserSettings As Class_TextFile
+    End Class
+End Module
